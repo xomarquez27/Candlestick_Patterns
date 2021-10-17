d4ad1fc82705225d823b4fa04e6a5b3f4aeea7b3
Xavier Otero Marquez <xomarquez27@gmail.com>
2021-10-16 17:34:28 -0700

    Refactored code to only post one pattern per day. This is done by setting the columns in a specific order which represents the importance of the pattern i.e. a Three White Soldiers pattern will appear ahead of Rising Price and Volume pattern in column order giving it more importance and priority.  This way the first pattern to be True has a higher weight than any subsequent patterns that may be true for the day. The Higher than Average Volume pattern has been modified to only be True when volume is higher than the 95th percentile while the Lower than Average volume pattern is true when volume is below the 5th percentile.

--- poster.py
+++ poster.py
@@ -43,41 +43,14 @@ if __name__ == '__main__':
             
             # Indicates if stock closed higher than day's open, not previous close.
             data['Day Close'] = np.where(data['Close'] > data['Open'], 'Up', 'Down')
-            
-
-            # Find hammer candlestick if open - low > (close - open) * 2 
-            # if the hammer's shadow is at least 2 times greater than the size of the real body
-            data['Hammer'] = np.where(data['Day Close'] == 'Up', (data['Open'] - data['Low'] >= ((data['High'] - data['Open']) * 2)) & (data['High'] - data['Close'] <= ((data['Close'] - data['Open'])* .25)), False)
-
-            data['Inverted Hammer'] = np.where(data['Day Close'] == 'Up', data['High'] - data['Close'] >= ((data['Close'] - data['Low']) * 2), False)
 
             data['Marubozu'] = np.where((data['Open'] == data['Low']) & (data['High'] == data['Close']), True, False)
 
-            data['Dragonfly Doji'] = np.where((data['High'] <= data['Open'] * 1.0015) & (data['Close'] >= data['Open'] * 0.9985), True, False)
-
-            data['Engulfing'] = np.where((data['Day Close'].shift(-1) == 'Down') & (data['Close'] > data['Open'].shift(-1)) & 
-                                        (data['Open'] < data['Close'].shift(-1)), True, False)
-
-            data['Piercing Line'] = np.where((data['Day Close'].shift(-1) == 'Down') & (data['Close'] > data['Close'].shift(-1) + 
-                                            ((data['Open'].shift(-1) - data['Close'].shift(-1)) * 0.50)) & 
-                                            (data['Close'] < data['Open'].shift(-1)) & (data['Open'] < data['Close'].shift(-1)), True, False)
-
-            data['Harami'] = np.where((data['Day Close'] == 'Up') & (data['Day Close'].shift(-1) == 'Down') &
-                                     (data['Low'] > data['Close'].shift(-1)) & (data['High'] < data['Open'].shift(-1)), True, False)
-
-            data['Three White Soldiers'] = np.where((data['Day Close'] == 'Up') & (data['Open'] > data['Open'].shift(-1)) & (data['Open'] < data['Close'].shift(-1)) & (data['Day Close'].shift(-1) == 'Up') & (data['Open'].shift(-1) > data['Open'].shift(-2)) & (data['Open'].shift(-1) < data['Close'].shift(-2)), True, False)
-
-            data['Morning Star'] = np.where((data['Day Close'] == 'Up') & (data['Day Close'].shift(-2) == 'Down') & 
-                                            (data['Day Close'].shift(-1) == 'Up') & 
-                                            (data['Close'].shift(-1) < data['Close'].shift(-2)) &
-                                            (data['Open'] > data['Close'].shift(-2)) &
-                                            (data['Close'] > data['Close'].shift(-2)), True, False)
-            
-      # Open is higher than any of the previous 20 highs and it closes above the previous close
-            data['Minor Breakout'] = data['Open'].gt(data['High'][::-1].shift().rolling(20).max()) & (data['Close'] > data['High'].shift(-1))
-            
-      # Open is higher than any of the previous 50 highs and it closes above the previous close
+            # Open is higher than any of the previous 50 highs and it closes above the previous close
             data['Major Breakout'] = data['Open'].gt(data['High'][::-1].shift().rolling(50).max()) & (data['Close'] > data['High'].shift(-1))
+
+            # Open is higher than any of the previous 20 highs and it closes above the previous close
+            data['Minor Breakout'] = data['Open'].gt(data['High'][::-1].shift().rolling(20).max()) & (data['Close'] > data['High'].shift(-1))
 
             data['Bull Flagpole'] = np.where((data['Day Close'] == 'Up') & 
                                             ((data['Close'] - data['Open']) >= (((((data['High'].shift(-1) - data['Low'].shift(-1)) +
@@ -85,69 +58,44 @@ if __name__ == '__main__':
                                             (data['High'].shift(-3) - data['Low'].shift(-3)))) / 3) * 2.5)) &
                                             (data['Open'] >= data['Low'].shift(-1)), True, False)
 
-      # Close is lower than any of the previous 20 lows
-            data['Minor Breakdown'] = data['Close'].lt(data['Low'][::-1].shift().rolling(20).min())
-            
-      # Close is lower than any of the previous 50 lows
-            data['Major Breakdown'] = data['Close'].lt(data['Low'][::-1].shift().rolling(50).min())
+            data['Three White Soldiers'] = np.where((data['Day Close'] == 'Up') & (data['Open'] > data['Open'].shift(-1)) & (data['Open'] < data['Close'].shift(-1)) & (data['Day Close'].shift(-1) == 'Up') & (data['Open'].shift(-1) > data['Open'].shift(-2)) & (data['Open'].shift(-1) < data['Close'].shift(-2)), True, False)
+
+            data['Piercing Line'] = np.where((data['Day Close'].shift(-1) == 'Down') & (data['Close'] > data['Close'].shift(-1) + 
+                                            ((data['Open'].shift(-1) - data['Close'].shift(-1)) * 0.50)) & 
+                                            (data['Close'] < data['Open'].shift(-1)) & (data['Open'] < data['Close'].shift(-1)), True, False)
+
+            data['Engulfing'] = np.where((data['Day Close'].shift(-1) == 'Down') & (data['Close'] > data['Open'].shift(-1)) & 
+                                        (data['Open'] < data['Close'].shift(-1)), True, False)
+
+            # Find hammer candlestick if open - low > (close - open) * 2 
+            # if the hammer's shadow is at least 2 times greater than the size of the real body
+            data['Hammer'] = np.where(data['Day Close'] == 'Up', (data['Open'] - data['Low'] >= ((data['High'] - data['Open']) * 2)) & (data['High'] - data['Close'] <= ((data['Close'] - data['Open'])* .25)), False)
+
+            data['Morning Star'] = np.where((data['Day Close'] == 'Up') & (data['Day Close'].shift(-2) == 'Down') & 
+                                            (data['Day Close'].shift(-1) == 'Up') & 
+                                            (data['Close'].shift(-1) < data['Close'].shift(-2)) &
+                                            (data['Open'] > data['Close'].shift(-2)) &
+                                            (data['Close'] > data['Close'].shift(-2)), True, False)
+
+            data['Harami'] = np.where((data['Day Close'] == 'Up') & (data['Day Close'].shift(-1) == 'Down') &
+                                     (data['Low'] > data['Close'].shift(-1)) & (data['High'] < data['Open'].shift(-1)), True, False)
+
+            data['Dragonfly Doji'] = np.where((data['High'] <= data['Open'] * 1.0015) & (data['Close'] >= data['Open'] * 0.9985), True, False)
 
             data['Bear Flagpole'] = np.where((data['Day Close'] == 'Down') &
                                             ((data['Open'] - data['Close']) >= ((((data['High'].shift(-1) - data['Low'].shift(-1)) +
                                             (data['High'].shift(-2) - data['Low'].shift(-2)) +
                                             (data['High'].shift(-3) - data['Low'].shift(-3))) / 3) * 2.5)) &
                                             (data['Open'] <= data['High'].shift(-1)), True, False)
+            # Close is lower than any of the previous 50 lows
+            data['Major Breakdown'] = data['Close'].lt(data['Low'][::-1].shift().rolling(50).min())
 
-            # Price and volume increase for the past 3 days 
-            data['Rising Price and Volume'] = np.where((data['Close'] > data['Close'].shift(-1)) &
-                                                        (data['Close'].shift(-1) > data['Close'].shift(-2)) &
-                                                        (data['Close'].shift(-1) > data['Close'].shift(-2)) &
-                                                        (data['Volume'] > data['Volume'].shift(-1)) &
-                                                        (data['Volume'].shift(-1) > data['Volume'].shift(-2)), True, False)
-
-            # Price increase, volume decrease for the past 3 days
-            data['Rising Price, Declining Volume'] = np.where((data['Close'] > data['Close'].shift(-1)) &
-                                                                (data['Close'].shift(-1) > data['Close'].shift(-2)) &
-                                                                (data['Close'].shift(-2) > data['Close'].shift(-3)) &
-                                                                (data['Volume'] < data['Volume'].shift(-1)) &
-                                                                (data['Volume'].shift(-1) < data['Volume'].shift(-2)), True, False)
-
-            # Volume decrease, price decrease for the past 3 days
-            data['Lower Prices Lower Volume'] = np.where((data['Close'] < data['Close'].shift(-1)) &
-                                                        (data['Close'].shift(-1) < data['Close'].shift(-2)) &
-                                                        (data['Close'].shift(-2) < data['Close'].shift(-3)) &
-                                                        (data['Volume'] < data['Volume'].shift(-1)) &
-                                                        (data['Volume'].shift(-1) < data['Volume'].shift(-2)), True, False)
-
-            # Price decrease, volume increase for the past 3 days
-            data['Lower Prices Higher Volume'] = np.where((data['Close'] < data['Close'].shift(-1)) &
-                                                          (data['Close'].shift(-1) < data['Close'].shift(-2)) &
-                                                          (data['Close'].shift(-2) < data['Close'].shift(-3)) &
-                                                          (data['Volume'] > data['Volume'].shift(-1)) &
-                                                          (data['Volume'].shift(-1) > data['Volume'].shift(-2)), True, False)
-
-            # Volume is greater than 2.5 times average daily volume
-            data['Above Avg Volume'] = np.where(data['Volume'] > ((data['Volume'].mean() * 2.5)), True, False)
-
-            # Volume is lower than half the average daily volume
-            data['Below Avg Volume'] = np.where(data['Volume'] < ((data['Volume'].mean() * 0.5)), True, False)
-
-
-            data['Hanging Man'] = np.where((data['Day Close'] == 'Down') & ((data['Close'] - data['Low']) >= ((data['Open'] - data['Close']) * 2)) &
-                                            ((data['High'] - data['Open']) < (data['Open'] - data['Close']) * 0.50),True, False)
+            # Close is lower than any of the previous 20 lows
+            data['Minor Breakdown'] = data['Close'].lt(data['Low'][::-1].shift().rolling(20).min())
 
             data['Bearish Engulfing'] = np.where((data['Day Close'].shift(-1) == 'Up') &
                                                 (data['Close'] < data['Open'].shift(-1)) &
                                                 (data['Open'] > data['Close'].shift(-1)), True, False)
-
-            data['Gravestone Doji'] = np.where((data['Low'] >= data['Open'] * 0.9985) &
-                                                (data['Close'] <= data['Open'] * 1.0015), True, False)
-
-            data['Bearish Harami'] = np.where((data['Day Close'] == 'Down') & (data['Day Close'].shift(-1) == 'Up') &
-                                             (data['High'] < data['Close'].shift(-1)) & (data['Low'] > data['Open'].shift(-1)), True, False)
-
-            data['Dark Cloud Cover'] = np.where((data['Day Close'] == 'Down') & (data['Day Close'].shift(-1) == 'Up') &
-                                                (data['Open'] > data['Close'].shift(-1)) & (data['Close'] >= (((data['Close'].shift(-1) -
-                                                data['Open'].shift(-1)) * .5) + data['Open'].shift(-1))) & (data['Close'] < data['Close'].shift(-1)), True, False)
 
             data['Three Black Crows'] = np.where((data['Day Close'] == 'Down') & (data['Open'] > data['Close'].shift(-1)) &
                                                 (data['Open'] < data['Open'].shift(-1)) & (data['Day Close'].shift(-1) == 'Down') &                                             (data['Close'] < data['Close'].shift(-1)) & (data['Day Close'].shift(-1) == 'Down') &
@@ -159,14 +107,55 @@ if __name__ == '__main__':
                                             (data['Close'].shift(-1) > data['Close'].shift(-2)) & 
                                             (data['Open'] < data['Close'].shift(-1)) & 
                                             (data['Close'] > data['Open'].shift(-2)), True, False)
+
+            data['Dark Cloud Cover'] = np.where((data['Day Close'] == 'Down') & (data['Day Close'].shift(-1) == 'Up') &
+                                                (data['Open'] > data['Close'].shift(-1)) & (data['Close'] >= (((data['Close'].shift(-1) -
+                                                data['Open'].shift(-1)) * .5) + data['Open'].shift(-1))) & (data['Close'] < data['Close'].shift(-1)), True, False)
+
+            data['Bearish Harami'] = np.where((data['Day Close'] == 'Down') & (data['Day Close'].shift(-1) == 'Up') &
+                                             (data['High'] < data['Close'].shift(-1)) & (data['Low'] > data['Open'].shift(-1)), True, False)
+
+            data['Inverted Hammer'] = np.where(data['Day Close'] == 'Up', data['High'] - data['Close'] >= ((data['Close'] - data['Low']) * 2), False)
+
+            data['Gravestone Doji'] = np.where((data['Low'] >= data['Open'] * 0.9985) &
+                                                (data['Close'] <= data['Open'] * 1.0015), True, False)
+            data['Hanging Man'] = np.where((data['Day Close'] == 'Down') & ((data['Close'] - data['Low']) >= ((data['Open'] - data['Close']) * 2)) &
+                                            ((data['High'] - data['Open']) < (data['Open'] - data['Close']) * 0.50),True, False)
+            # Price and volume increase for the past 3 days 
+            data['Rising Price and Volume'] = np.where((data['Close'] > data['Close'].shift(-1)) &
+                                                        (data['Close'].shift(-1) > data['Close'].shift(-2)) &
+                                                        (data['Close'].shift(-1) > data['Close'].shift(-2)) &
+                                                        (data['Volume'] > data['Volume'].shift(-1)) &
+                                                        (data['Volume'].shift(-1) > data['Volume'].shift(-2)), True, False)
+            # Price increase, volume decrease for the past 3 days
+            data['Rising Price, Declining Volume'] = np.where((data['Close'] > data['Close'].shift(-1)) &
+                                                                (data['Close'].shift(-1) > data['Close'].shift(-2)) &
+                                                                (data['Close'].shift(-2) > data['Close'].shift(-3)) &
+                                                                (data['Volume'] < data['Volume'].shift(-1)) &
+                                                                (data['Volume'].shift(-1) < data['Volume'].shift(-2)), True, False)
+            # Price decrease, volume increase for the past 3 days
+            data['Lower Prices Higher Volume'] = np.where((data['Close'] < data['Close'].shift(-1)) &
+                                                          (data['Close'].shift(-1) < data['Close'].shift(-2)) &
+                                                          (data['Close'].shift(-2) < data['Close'].shift(-3)) &
+                                                          (data['Volume'] > data['Volume'].shift(-1)) &
+                                                          (data['Volume'].shift(-1) > data['Volume'].shift(-2)), True, False)
+            # Volume decrease, price decrease for the past 3 days
+            data['Lower Prices Lower Volume'] = np.where((data['Close'] < data['Close'].shift(-1)) &
+                                                        (data['Close'].shift(-1) < data['Close'].shift(-2)) &
+                                                        (data['Close'].shift(-2) < data['Close'].shift(-3)) &
+                                                        (data['Volume'] < data['Volume'].shift(-1)) &
+                                                        (data['Volume'].shift(-1) < data['Volume'].shift(-2)), True, False)
+            # Volume is greater than 2.5 times average daily volume
+            data['Above Avg Volume'] = np.where(data['Volume'] >= ((data['Volume'].describe(percentiles=[.95])[5]), True, False)
+            # Volume is lower than half the average daily volume
+            data['Below Avg Volume'] = np.where(data['Volume'] <= ((data['Volume'].describe(percentiles=[.05])[5])), True, False)
             
-            day_pattern = [data.columns[(data == True).iloc[0]]][0]
+            day_pattern = [data.columns[(data == True).iloc[0]]][0][0]
             content = []
             if day_pattern.empty:
                 pass
             else:
                 for i in day_pattern:
-
                     # Dataframe with all days in which the pattern is True
                     performance = pd.DataFrame(data.loc[data[i] == True])
                     # Grab the close from 5 days after a pattern occurs and make result be the difference in price
@@ -174,7 +163,6 @@ if __name__ == '__main__':
                     performance['Five Days'] = data.shift(5)[data[i] == True]['Close']
                     performance['Result'] =  performance['Five Days'] - performance['Close']
                     stats = performance['Result'].describe(percentiles=[.95])
-
                     content.append(f'${file.split(".csv")[0]} {i}\n')
                     content.append(f'Daily appearances since {data.iloc[-1,0].year}: {int(stats[0])}\n')
                     content.append(f"Close 5 days later \u00B1:\n")
@@ -182,7 +170,6 @@ if __name__ == '__main__':
                     content.append(f"SD: {stats[2]:.2f}\n")
                     content.append(f"Worst: {stats[3]:.2f}\n")
                     content.append(f"Best: {stats[6]:.2f}\n\n")
-
 
                     # Same concept as above but using 10 days after, then 30 days
                     performance['Ten Days'] = data.shift(10)[data[i] == True]['Close']
